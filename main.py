f8a178f9bf8768662f926b6b3f30f34b0a6687c0
2002Quang <133742839+2002Quang@users.noreply.github.com>
2023-05-16 13:55:24 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+from scheduler import *
+from task1 import *
+from task2 import *
+scheduler = Scheduler()
+scheduler.SCH_Init()
+
+task1 = Task1()
+task2 = Task2()
+
+task1.Task1_Run()
+task2.Task2_Run()
