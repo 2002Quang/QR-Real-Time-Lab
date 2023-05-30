7fd20990c89060048b3f39f814586299d7a0929c
Triet20 <133742997+Triet20@users.noreply.github.com>
2023-05-30 10:10:04 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,6 @@
+import tensorflow as tf
+import numpy as np
+
 class Task1:
     def __init__(self):
         print("Initiate task 1")
@@ -5,3 +8,16 @@ class Task1:
 
     def Task1_Run(self):
         print("Task 1 is activated")
+
+    def classify_image(self, image_path):
+        model = tf.keras.models.load_model('C:/Users/Administrator/Desktop/Subjects/Realtime System/keras_model.h5')
+        
+        img = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = tf.keras.applications.resnet50.preprocess_input(img_array)
+        
+        prediction = model.predict(img_array)
+        predicted_class = tf.keras.applications.resnet50.decode_predictions(prediction, top=1)[0][0][1]
+        
+        return predicted_class
