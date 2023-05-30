862554cc6679a5aeea85297d78b39f16571794e3
Triet20 <133742997+Triet20@users.noreply.github.com>
2023-05-30 10:34:37 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,35 +1,39 @@
 import tensorflow as tf
 import numpy as np
-from PIL import Image
+from keras.models import load_model
+import cv2
 
 class Task1:
-    def __init__(self, index):
-        print("Init task 1")
+    def __init__(self,index):
+        print("Initiate task 1")
         np.set_printoptions(suppress=True)
 
-        # Load the model
-        self.model = load_model("keras_Model.h5", compile=False)
+        self.model = load_model("keras_model.h5", compile = False)
+        self.class_names = open("prediction.txt", "r").readlines()
 
-        # Load the labels
-        self.class_names = open("labels.txt", "r").readlines()
-
-        # CAMERA can be 0 or 1 based on default camera of your computer
         self.camera = cv2.VideoCapture(index)
-        self.cameraID =  index
+        self.cameraID = index
         return
 
-    def Task1_Run(self):
-        print("Task 1 is activated")
-        
-    def classify_image(self, image_path):
-        model = tf.keras.models.load_model('C:/Users/Administrator/Desktop/Subjects/Realtime System/keras_model.h5')
-        
-        img = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
-        img_array = tf.keras.preprocessing.image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array = tf.keras.applications.resnet50.preprocess_input(img_array)
-        
-        prediction = model.predict(img_array)
-        predicted_class = tf.keras.applications.resnet50.decode_predictions(prediction, top=1)[0][0][1]
-        
-        return predicted_class
+    def Task1Run(self):
+        print("Task1 is activated")
+       
+        ret, image = self.camera.read()
+
+        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+        cv2.imshow("Webcam Image", image)
+
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+        image = (image / 127.5) - 1
+
+        prediction = self.model.predict(image)
+        index = np.argmax(prediction)
+        class_name = self.class_names[index]
+        confidence_score = prediction[0][index]
+
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+        keyboard_input = cv2.waitKey(1)
