9ac69e0184e97d7e3e204533d25b96810a4053e8
Triet20 <133742997+Triet20@users.noreply.github.com>
2023-05-30 14:14:38 +0700

    Task 3 (2 cameras)

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import time
-from scheduler import *
+import scheduler
 from task1 import *
 from task2 import *
-scheduler = Scheduler()
+from Task3 import *
+
+scheduler = scheduler.Scheduler()
 scheduler.SCH_Init()
+#task1 = Task1(0)
+# task2 = Task2()
+task3 = Task3(0)
+task3cam2 = Task3(1)
 
-task1 = Task1()
-task1.Task1_Run()
-predicted_class = task1.classify_image('C:/Users/Administrator/Desktop/Subjects/Realtime System/student.jpg')
-print(predicted_class)
-task2 = Task2()
-
-scheduler.SCH_Add_Task(task1.Task1_Run, 1000, 2000)
-scheduler.SCH_Add_Task(task2.Task2_Run, 2000, 4000)
-
+#scheduler.SCH_Add_Task(task1.Task1Run, 1000, 2000)
+scheduler.SCH_Add_Task(task3.Task3Run, 2000, 4000)
+scheduler.SCH_Add_Task(task3cam2.Task3Run, 2000, 4000)
 while True:
     scheduler.SCH_Update()
     scheduler.SCH_Dispatch_Tasks()
     time.sleep(1)
+
+task1b.camera.release()
+task1a.camera.release()
+cv2.destroyAllWindows()
