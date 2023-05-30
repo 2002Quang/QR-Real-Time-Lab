e113f3724a594743f0b32f6952dc61baf6fd3ede
2002Quang <133742839+2002Quang@users.noreply.github.com>
2023-05-30 10:17:28 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,13 +3,24 @@ import numpy as np
 from PIL import Image
 
 class Task1:
-    def __init__(self):
-        print("Initiate task 1")
+    def __init__(self, index):
+        print("Init task 1")
+        np.set_printoptions(suppress=True)
+
+        # Load the model
+        self.model = load_model("keras_Model.h5", compile=False)
+
+        # Load the labels
+        self.class_names = open("labels.txt", "r").readlines()
+
+        # CAMERA can be 0 or 1 based on default camera of your computer
+        self.camera = cv2.VideoCapture(index)
+        self.cameraID =  index
         return
 
     def Task1_Run(self):
         print("Task 1 is activated")
-
+        
     def classify_image(self, image_path):
         model = tf.keras.models.load_model('C:/Users/Administrator/Desktop/Subjects/Realtime System/keras_model.h5')
         
