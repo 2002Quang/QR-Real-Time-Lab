a337159bc78ff9c60b44a14800408d0448571150
Triet20 <133742997+Triet20@users.noreply.github.com>
2023-05-30 10:10:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ scheduler = Scheduler()
 scheduler.SCH_Init()
 
 task1 = Task1()
+task1.Task1_Run()
+predicted_class = task1.classify_image('C:/Users/Administrator/Desktop/Subjects/Realtime System/student.jpg')
+print(predicted_class)
 task2 = Task2()
 
 scheduler.SCH_Add_Task(task1.Task1_Run, 1000, 2000)
-scheduler.SCH_Add_Task(task2.Task1_Run, 2000, 4000)
+scheduler.SCH_Add_Task(task2.Task2_Run, 2000, 4000)
 
 while True:
     scheduler.SCH_Update()
